448169b0d4e05884281d2df5c70e1e3d5c38bb59
chengzihang <chengzihang@geyecloud.com>
2022-06-08 21:01:24 +0800

    login static css style

--- vue3_admin_system/src/views/Login.vue
+++ vue3_admin_system/src/views/Login.vue
@@ -1,5 +1,5 @@
 <script>
-import { getCurrentInstance, onMounted } from "@vue/runtime-core";
+import { getCurrentInstance, reactive, ref } from "@vue/runtime-core";
 import { useRouter } from "vue-router";
 import Welcome from "../views/Welcome.vue";
 
@@ -11,23 +11,46 @@ export default {
   setup() {
     const router = useRouter();
     const internalInstance = getCurrentInstance();
-    const request =
-      internalInstance.appContext.config.globalProperties.$request;
 
-    const goHome = () => {
-      router.push("/welcome");
-    };
+    const userFormRef = ref(null);
 
-    onMounted(() => {
-      request
-        .get("/login", { name: "michael" }, { mock: true, loading: true })
-        .then((res) => {
-          console.log("res", res);
-        });
+    const user = reactive({
+      userName: "",
+      userPwd: "",
     });
 
+    const rules = reactive({
+      userName: [
+            { required: true, message: 'Please type in you username', trigger: 'blur' },
+            { min: 3, max: 15, message: 'Length should be 3 to 15', trigger: 'blur' },
+      ],
+      userPwd: [
+        {
+          required: true,
+          message: "Please type your password",
+          trigger: "blur",
+        },
+      ],
+    });
+
+    const login = async (formEl) => {
+      if(!formEl) return
+      await formEl.validate((valid, field) => {
+        if(valid) {
+          console.log('submit', formEl);
+        } else {
+          console.log('error submit', field);
+        }
+      })
+    };
+
     return {
-      goHome,
+      userFormRef,
+
+      user,
+      rules,
+
+      login,
     };
   },
 };
@@ -36,16 +59,28 @@ export default {
 <template>
   <div class="login-wrapper">
     <div class="modal">
-      <el-form>
+      <!-- :model 表单公共的作用域（user这个对象），v-model绑定每一个表单的对象 -->
+      <el-form
+        ref="userFormRef"
+        :model="user"
+        status-icon
+        :rules="rules"
+      >
         <div class="title">Log In</div>
-        <el-form-item>
-          <el-input type="text" prefix-icon="el-icon-user" />
+        <el-form-item prop="userName">
+          <el-input
+            type="text"
+            v-model="user.userName"
+          />
+        </el-form-item>
+        <el-form-item prop="userPwd">
+          <el-input
+            type="password"
+            v-model="user.userPwd"
+          />
         </el-form-item>
         <el-form-item>
-          <el-input type="password" prefix-icon="el-icon-view" />
-        </el-form-item>
-        <el-form-item>
-          <el-button type="primary" class="btn-login" >Login</el-button>
+          <el-button type="primary" class="btn-login" @click="login(userFormRef)" >Login</el-button>
         </el-form-item>
       </el-form>
     </div>
@@ -53,24 +88,27 @@ export default {
 </template>
 
 <style lang="scss">
-.login-wrapper{
+.login-wrapper {
   display: flex;
   justify-content: center;
   align-items: center;
   background-color: #f9fcff;
   width: 100vw;
   height: 100vh;
-  .modal{
+  .modal {
     width: 500px;
     padding: 50px;
     background-color: white;
     border-radius: 4px;
     box-shadow: 0 0 10px 3px #c7c9cb4d;
-    .title{
+    .title {
       font-size: 50px;
       line-height: 1.5;
       text-align: center;
       margin-bottom: 30px;
+    }
+    .btn-login{
+      width: 100%;
     }
   }
 }
