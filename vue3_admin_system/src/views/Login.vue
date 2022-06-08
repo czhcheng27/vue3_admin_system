aa255c03d47835f8f165b004d4c142053c5f4efb
chengzihang <chengzihang@geyecloud.com>
2022-06-08 13:26:45 +0800

    login 1st static component

--- vue3_admin_system/src/views/Login.vue
+++ vue3_admin_system/src/views/Login.vue
@@ -34,9 +34,44 @@ export default {
 </script>
 
 <template>
-  <h3>Login Page</h3>
-  <el-button @click="goHome">Go Back to Home Page</el-button>
+  <div class="login-wrapper">
+    <div class="modal">
+      <el-form>
+        <div class="title">Log In</div>
+        <el-form-item>
+          <el-input type="text" prefix-icon="el-icon-user" />
+        </el-form-item>
+        <el-form-item>
+          <el-input type="password" prefix-icon="el-icon-view" />
+        </el-form-item>
+        <el-form-item>
+          <el-button type="primary" class="btn-login" >Login</el-button>
+        </el-form-item>
+      </el-form>
+    </div>
+  </div>
 </template>
 
-<style scoped>
+<style lang="scss">
+.login-wrapper{
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background-color: #f9fcff;
+  width: 100vw;
+  height: 100vh;
+  .modal{
+    width: 500px;
+    padding: 50px;
+    background-color: white;
+    border-radius: 4px;
+    box-shadow: 0 0 10px 3px #c7c9cb4d;
+    .title{
+      font-size: 50px;
+      line-height: 1.5;
+      text-align: center;
+      margin-bottom: 30px;
+    }
+  }
+}
 </style>
